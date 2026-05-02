f1b06ed0fb11e6bff736ce850b5c5731a9b23d98
Ayusht20 <ayushtrilokchandani2005@gmail.com>
2026-05-02 13:19:39 +0530

    Changes made

--- backend/main.py
+++ backend/main.py
@@ -57,7 +57,7 @@ def get_db():
     try:
         return mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            port=int(os.getenv("DB_PORT", 3306))
+            port=int(os.getenv("DB_PORT", 3306)),
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASS"),
             database=os.getenv("DB_NAME")
